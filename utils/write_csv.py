192c6163025b7dcddb92d5d8e0fa49bb5bd72a26
wangdaqiao <mswangjian+github@gmail.com>
2024-04-27 18:01:17 +0800

    csv DictWriter -> add 'extrasaction' param

--- utils/write_csv.py
+++ utils/write_csv.py
@@ -17,9 +17,23 @@ def record_request_csv(csvfile_path=None, csv_row_data: dict=None, csv_headers: 
         return
     try:
         with open(csvfile_path, "a+", encoding='utf-8', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, csv_headers)
+            writer = csv.DictWriter(csvfile, fieldnames=csv_headers, extrasaction='ignore')
             if csvfile.tell() == 0:
                 writer.writeheader()
             writer.writerow(csv_row_data)
     except Exception as err:
         logger.error(err)
+
+
+if __name__ == '__main__':
+    import json
+    csvheaders = ['request_url', 'payload', 'length']
+
+    request_url1 = '/v1/getNewUpdatesList'
+    payload_dct1 = {"start": "0", "limit": "10", "offset": False, "selectedId": None}
+    payload_str1 = json.dumps(payload_dct1, indent=None, ensure_ascii=False)
+    api_content1 = [request_url1, payload_str1, 144]
+    res_info_dct1 = dict(zip(csvheaders, api_content1))
+    logger.info(res_info_dct1)
+    record_request_csv(csvfile_path='demo_write.csv', csv_row_data=res_info_dct1, csv_headers=csvheaders)
+    record_request_csv(csvfile_path='demo_write.csv', csv_row_data=res_info_dct1, csv_headers=['request_url', 'payload'])
