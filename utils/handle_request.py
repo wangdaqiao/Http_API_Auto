cc8f1591cb6c1a5643d90fe7f74646c4066ebcdb
wangdaqiao <mswangjian+github@gmail.com>
2024-04-27 18:04:51 +0800

    1. update skip test cases if var not extracted in the previous api 2. add some pytest cases for replace vars in a string

--- utils/handle_request.py
+++ utils/handle_request.py
@@ -30,8 +30,12 @@ def request_url_var_to_str(request_url: str = '', vars_dct: dict = {}) -> str:
     Returns:
         str: The request URL with variables replaced.
     """
-    if '${' in request_url:
+    if '${' not in request_url:
+        return request_url
+    try:
         request_url = Template(request_url).substitute(vars_dct)
+    except Exception as err:
+        pytest.skip(f'KeyError {err} , because it was not extracted in the previous api\nThe {request_url=}\n{vars_dct=}')
     return request_url
 
 
@@ -49,20 +53,24 @@ def payload_vars_to_dct(payload_str: str = '', vars_dct: dict = {}) -> dict:
     if '${' not in payload_str:
         return json.loads(payload_str)
     try:
-        string_text: str = Template(payload_str).safe_substitute(vars_dct)
+        string_text: str = Template(payload_str).substitute(vars_dct)
+    except Exception as err:
+        logger.error(err)
+        pytest.skip(f'KeyError {err} , because it was not extracted in the previous api\nThe payload is: {payload_str}\n{vars_dct=}')
+        return
+    try:
         payload_dict = json.loads(string_text)
     except json.decoder.JSONDecodeError as err:
-        logger.exception(err)
+        logger.error(err)
         logger.error(f'{payload_str=}')
         logger.error(f'{vars_dct=}')
         logger.error(f'{string_text=}')
         try:
             payload_dict = literal_eval(string_text)
-        except NameError as err:
+        except Exception as err:
             payload_dict = string_text
-            logger.exception(err)
+            logger.error(err)
     finally:
-        logger.debug(f'{payload_dict=}')
         return payload_dict
 
 
@@ -95,18 +103,9 @@ def preprocess_send_request(case_data, email=None, password=None, vars_dct=None)
     # params_dict
     params_dict = payload_vars_to_dct(payload_str=params_str, vars_dct=vars_dct)
     logger.debug(f'{params_dict=}')
-    if params_str and '${' in params_str:
-        for k, v in vars_dct.items():
-            if k in params_str and not v:
-                pytest.skip(f'skip api {request_url=} due to null data in params.\n The params is: {params_dict}')
     # payload_dict
     payload_dict = payload_vars_to_dct(payload_str=payload_str, vars_dct=vars_dct)
     logger.debug(f'{payload_dict=}')
-    if payload_str and payload_dict and '${' in payload_str:
-        for k, v in vars_dct.items():
-            if k in payload_str and not v:
-                pytest.skip(
-                    f'skip api {request_url} due to null data in payload. \n The payload is : {payload_dict}')
     # http request
     obj = AppApiBase(url=request_url, http_method=http_method, data_type=data_type, params_dict=params_dict,
                      payload_dict=payload_dict, files=files, email=email, password=password)
@@ -143,36 +142,56 @@ def extract_vars(response_data=None, var_extract: str = '', vars_dct: dict | Non
     return vars_dct
 
 
+class TestSuite:
+    def test_01(self):
+        reminder_id = [111, 333]
+        timsstamp_10days_late = 222
+        vars_dct = {'reminder_id': reminder_id, 'timsstamp_10days_late': timsstamp_10days_late}
+        data_str = '{"reminderId":${reminder_id},"scheduleTimestamp":${timsstamp_10days_late},"note":"reminder note2"}'
+        aa = payload_vars_to_dct(payload_str=data_str, vars_dct=vars_dct)
+        logger.debug(f'return {aa}')
+        logger.debug(type(aa))
+        assert aa == {'reminderId': [111, 333], 'scheduleTimestamp': 222, 'note': 'reminder note2'}
+
+    def test_02(self):
+        data_str = '${user_email}'
+        vars_dct = {'user_email': 'abcd@aa.info'}
+        bb = payload_vars_to_dct(payload_str=data_str, vars_dct=vars_dct)
+        # logger.debug(bb)
+        logger.debug(f'return {bb}')
+        logger.debug(type(bb))
+        assert bb == 'abcd@aa.info'
+
+    def test_03(self):
+        payload_str = '{"recipients":[{"email":"${mail_receiver}","name":"testmail"}],"message":"somethingoptionalmessage","configuration":"{\\"currentStep\\":2,\\"userId\\":\\"${userId}\\",\\"singleCompany\\":true}}","company":"Microsoft","titles":"JavaDeveloper"}'
+        vars_dct = {'userId': 'abcd1234', 'firstname': 'T', 'history_id': 422776, 'mail_receiver': 'cdccdd@ccc.inf'}
+        cc = payload_vars_to_dct(payload_str=payload_str, vars_dct=vars_dct)
+        # logger.debug(cc)
+        logger.debug(f'return {cc}')
+        logger.debug(type(cc))
+
+    def test_04(self):
+        payload_str = '[]'
+        vars_dct = {}
+        dd = payload_vars_to_dct(payload_str=payload_str, vars_dct=vars_dct)
+        logger.debug(f'return {dd}')
+        logger.debug(type(dd))
+        assert dd == []
+
+    def test_05(self):
+        data_str = '${user_email}'
+        vars_dct = {}
+        ee = payload_vars_to_dct(payload_str=data_str, vars_dct=vars_dct)
+        logger.debug(f'return {ee}')
+        logger.debug(type(ee))
+        assert ee is None
+
+    def test_06(self):
+        vars_dct = {'userId': 'abcd1234', 'firstname': 'T', 'history_id': 422777, 'mail_receiver': 'cdccdd@ccc.inf'}
+        url = request_url_var_to_str(request_url='/v1/${history_id}/history', vars_dct=vars_dct)
+        logger.debug(f'{url=}')
+        assert url == '/v1/422777/history'
+
+
 if __name__ == '__main__':
-    reminder_id = [111, 333]
-    timsstamp_10days_late = 222
-    vars_dct = {'reminder_id': reminder_id, 'timsstamp_10days_late': timsstamp_10days_late}
-    data_str = '{"reminderId":${reminder_id},"scheduleTimestamp":${timsstamp_10days_late},"note":"reminder note2"}'
-    aa = payload_vars_to_dct(payload_str=data_str, vars_dct=vars_dct)
-    logger.debug(aa)
-    logger.debug(type(aa))
-    assert aa == {'reminderId': [111, 333], 'scheduleTimestamp': 222, 'note': 'reminder note2'}
-
-    data_str = '${user_email}'
-    vars_dct = {'user_email': 'abcd@aa.info'}
-    bb = payload_vars_to_dct(payload_str=data_str, vars_dct=vars_dct)
-    logger.debug(bb)
-    logger.debug(type(bb))
-    assert bb == 'abcd@aa.info'
-
-    payload_str = '{"recipients":[{"email":"${mail_receiver}","name":"testmail"}],"message":"somethingoptionalmessage","configuration":"{\\"currentStep\\":2,\\"userId\\":\\"${userId}\\",\\"singleCompany\\":true}}","company":"Microsoft","titles":"JavaDeveloper"}'
-    vars_dct = {'userId': 'abcd1234', 'firstname': 'T', 'history_id': 422776, 'mail_receiver': 'cdccdd@ccc.inf'}
-    cc = payload_vars_to_dct(payload_str=payload_str, vars_dct=vars_dct)
-    logger.debug(cc)
-    logger.debug(type(cc))
-
-    payload_str = '[]'
-    dd = payload_vars_to_dct(payload_str=payload_str, vars_dct=vars_dct)
-    logger.debug(dd)
-    logger.debug(type(dd))
-    assert dd == []
-
-    vars_dct = {'userId': 'abcd1234', 'firstname': 'T', 'history_id': 422777, 'mail_receiver': 'cdccdd@ccc.inf'}
-    url = request_url_var_to_str(request_url='/v1/${history_id}/history', vars_dct=vars_dct)
-    logger.debug(f'{url=}')
-    assert url == '/v1/422777/history'
+    pytest.main(['-s', '-v'])
